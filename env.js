2a37467ed210f5e1cedd64a5dc8f4ba3ef639461
Adit <dokteraditardhi@gmail.com>
2026-08-22 14:21:26 +0700

    Feat: Integrasi database cloud Supabase dengan realtime sync

---
+++ env.js
@@ -0,0 +1,5 @@
+// File konfigurasi Supabase (dapat disesuaikan secara lokal)
+window.SUPABASE_CONFIG = {
+    SUPABASE_URL: "https://olufbirzkfrmufpubeyb.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sdWZiaXJ6a2ZybXVmcHViZXliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczNzc0ODQsImV4cCI6MjEwMjk1MzQ4NH0.C8M74yxGKMcty8oIT5Isi_-c-ICSW3DeEs3PGyhe8Nk"
+};
